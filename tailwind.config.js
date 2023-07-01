da19f1d2479df984d47797b5556f6dc6dcf8b9b9
Tubo-Ibim Braide <40839135+iclectic@users.noreply.github.com>
2023-07-01 00:47:51 +0000

    15.34

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+const { fontfamily } = require(`tailwindcss/defaultTheme`)
+
+
+
 module.exports = {
   content: [
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mont: ['var(--font-mont)'],
+      },
+    },
   },
   plugins: [],
 }
