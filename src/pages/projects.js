59b3c55d29aae4e899244bca0f05455935924a19
iclectic <tansonestost@yahoo.com>
2023-12-17 23:08:12 -0600

    17.12.2022-I

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,7 +6,9 @@ import Link from 'next/link'
 import { GithubIcon } from '@/components/Icons'
 import Image from 'next/image'
 import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg";
+import { motion } from 'framer-motion';
 
+const FramerImage = motion(Image);
 
 const FeaturedProject = ({type, title, summary, img, link, github}) => {
 
@@ -22,7 +24,10 @@ const FeaturedProject = ({type, title, summary, img, link, github}) => {
           target="_blank"
           className='w-1/2 cursor-pointer overflow-hidden rounded-lg'
         >
-            <Image src={img} alt={title} className="w-full h-auto" />
+            <FramerImage src={img} alt={title} className="w-full h-auto" 
+            whileHover={{scale:1.05}}
+            transition={{duration:0.2}}
+            />
         </Link>  
 
         <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
@@ -58,7 +63,10 @@ const Project = ({title, type, img, link, github}) => {
              target="_blank"
              className='w-full cursor-pointer overflow-hidden rounded-lg'
         >
-            <Image src={img} alt={title} className="w-full h-auto" />
+            <FramerImage src={img} alt={title} className="w-full h-auto"
+             whileHover={{scale:1.05}}
+             transition={{duration:0.2}}
+            />
         </Link>  
 
         <div className="w-full flex flex-col items-start justify-between mt-4">
