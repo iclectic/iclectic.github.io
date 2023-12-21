9f995084b17efa479c978dcbaa3ebb68ae2391d0
iclectic <tansonestost@yahoo.com>
2023-12-21 03:36:22 +0000

    21.12.2023

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, {useState} from 'react'
 import Logo from './Logo'
 import { TwitterIcon, DribbbleIcon, GithubIcon, LinkedInIcon, PinterestIcon, MoonIcon } from './Icons'
 import {motion} from "framer-motion";
@@ -35,12 +35,25 @@ const NavBar = () => {
 
     
   const [mode, setMode] = useThemeSwitcher(); 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen)
+  }
 
   return (
     <header
     className="w-full px-32 py-8 font-medium flex items-center justify-between
     dark:text-light
     ">
+    <button className="flex flex-col justify-center items-center" onClick={handleClick}>
+        <span className={`bg-dark dark:bg-light block h-0.5 w-6 rounded-sm ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`}></span>
+        <span className={`bg-dark dark:bg-light block h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0' : 'opacity-100'} `}></span>
+        <span className={`bg-dark dark:bg-light block h-0.5 w-6 rounded-sm ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'} `}></span>
+
+    </button>
+
+
         <nav>
             <CustomLink href="/" title="Home" className='mr-4'/>
             <CustomLink href="/about" title="About" className='mx-4'/>
@@ -49,10 +62,10 @@ const NavBar = () => {
         </nav>
        
         <nav className="flex items-center justify-center flex-wrap">
-            <motion.a href="https://twitter.com" target={"_blank"}
+            <motion.a href="https://twitter.com" target={"_blank"}  className="w-6 mx-3 bg-light rounded-full"
             whileHover={{y:-2}}
             whileTap={{scale:0.9}}
-            className="w-6 mx-3"
+           
             >
                 <TwitterIcon />
             </motion.a>
@@ -77,7 +90,7 @@ const NavBar = () => {
             >
                 <GithubIcon />
             </motion.a>
-            <motion.a href="https://linkedin.com" target={"_blank"}
+            <motion.a href="https://linkedin.com" target={"_blank"} 
             whileHover={{y:-2}}
             whileTap={{scale:0.9}}
             className="w-6 ml-3"
